33a2bf0277eb102842bfe57c21defa36306e6f71
Daniel <danwebmaster95@gmail.com>
2017-01-17 20:39:52 +0100

    Subida inicial de archivos

---
+++ Assets/Scripts/DragonScript.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragonScript : MonoBehaviour {
+	 
+	private Rigidbody2D myRigidBody; //para que vuele
+	private Animator myAnimator; //para animar el dragón
+	private float jumpForce; //fuerza de salto
+	public bool isAlive; //si está vivo
+
+	void Start () {
+		isAlive = true; //está vivo el dragon
+
+		myRigidBody = gameObject.GetComponent<Rigidbody2D>();
+		myAnimator = gameObject.GetComponent<Animator> ();
+
+		jumpForce = 10f; //fuerza de salto
+		myRigidBody.gravityScale = 5f; //escala de gravedad
+
+	}
+		
+
+	// Update is called once per frame
+	void Update () {
+		if (isAlive) {
+			if (Input.GetMouseButton (0)) { //está función, propia de Unity, checkea si hay un click del mouse o toque en la pantalla
+				Flap (); //llama a la función que pone el trigger
+			} 
+			CheckIfDragonVisibleOnScreen ();
+		} 
+	}
+
+
+	void Flap(){
+		myRigidBody.velocity = 
+			new Vector2 (0,jumpForce);
+		
+		myAnimator.SetTrigger ("Flap"); //uso del trigger
+
+	}
+
+
+	void OnCollisionEnter2D(Collision2D target) { //función de colisión
+		if (target.gameObject.tag == "Obstacles") {
+			isAlive = false; //mata al dragón
+			Time.timeScale = 0f; //poner a cero por el fin de juego
+		}
+	}
+
+	void CheckIfDragonVisibleOnScreen() {
+		if (Mathf.Abs(gameObject.transform.position.y) > 5.3f) {
+			isAlive = false;
+			Time.timeScale = 0f;
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/ObstacleScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleScript : MonoBehaviour {
+
+
+	Rigidbody2D myRigidbody;
+	float dragonXposition;
+	bool isScoreAdded;
+
+	GameManagerScript gameManager;
+
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = gameObject.GetComponent<Rigidbody2D> (); //como en el dragón
+		myRigidbody.velocity = new Vector2 (-2.5f,0); //velocidad negativa (para mover a la izquierda, pero solo se mueve en el eje x)
+
+		dragonXposition = 
+			GameObject.Find("Dragon").transform.position.x; //busca la posición del dragón
+
+		isScoreAdded = false; //evita que se añada la puntuación múltiples veces
+
+		gameManager = GameObject.Find ("GameManager")
+			.GetComponent<GameManagerScript> ();
+	}
+
+	void Update() { //por cada frame hay que comprobar'si pasa el dragón (comprobar !isScoreAdded)
+		if (transform.position.x <= dragonXposition) { //mira si la posición x del dragón es mayor o igual que la del obstáculo (está escrito al revés)
+			if (!isScoreAdded) {
+				AddScore ();
+				isScoreAdded = true;
+			}
+		}
+
+		if (transform.position.x <= -10f) {
+			Destroy (gameObject); //destruye el objeto
+		}
+	}
+
+	void AddScore(){
+		gameManager.GmAddScore();
+	}
+
+}
